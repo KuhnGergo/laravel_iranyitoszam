b4fd7128191dc0de7bf4827cc3e5dfdbb56b0d5c
Your Name <kissmarci555@gmail.com>
2025-11-17 09:04:54 +0100

    init

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $response = Http::api()->post('/user/login', [
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        if ($response->successful()) {
+            // $response_body = json_decode($response['body']);
+            $token = $response['token'];
+            $user = $response['user'];
+            session([
+                'api_token' => $token,
+                'user_name' => $user['name'],
+                'user_email' => $user['email'],
+            ]);
+
+        }
+        return redirect()->intended('/');
+
+        // $request->authenticate();
+
+        // $request->session()->regenerate();
+
+        // return redirect()->intended(route('dashboard', absolute: false));
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        session()->forget('api_token');
+
+        return redirect('/');
+
+        // Auth::guard('web')->logout();
+
+        // $request->session()->invalidate();
+
+        // $request->session()->regenerateToken();
+
+        // return redirect('/');
+    }
+}
